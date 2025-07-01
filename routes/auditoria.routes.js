d06209f42e94ef7a41b5569f1c4e20f638c438f4
alexluna02 <alexlunamay2002@gmail.com>
2025-06-30 21:38:20 -0500

    Limpieza de datos api

--- routes/auditoria.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auditoriaController = require('../controllers/auditoria.controller');
-
-// Endpoints para auditoría
-router.get('/', auditoriaController.getAllAuditoria);
-router.get('/:id', auditoriaController.getAuditoriaById);
-//Export
-module.exports = router;
